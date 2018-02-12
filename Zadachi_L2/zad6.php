47bff7ddb88990e5829201f239625923b2e969c8
Georgi Georgiev <jorolin1999@gmail.com>
2018-02-12 08:19:35 +0200

    Update zad6.php

--- Zadachi_L2/zad6.php
+++ Zadachi_L2/zad6.php
@@ -14,14 +14,13 @@
                 <button type="submit"  value="Submit">Submit</button> 
             </form>        
     <?php
-        $a=0;
-        $b=0;
-        $c=0;
-        $x=0;
-        if($_POST){
+       if(!$_POST){
+           echo "Няма въведена инфoрмация!";
+       }
+        elseif($_POST){
             $a=$_POST["st1"];
             $b=$_POST["st2"];
-            $c=$_POST["st3"];}
+            $c=$_POST["st3"];
             if($c < $a && $c > $b){
                 $x = ($a * $b) - $c;
                 echo "Стойността на x e $x";
@@ -37,6 +36,7 @@
             $x = $a + $b - $c;
             echo "Стойността на x e $x";
         }
+        };
     ?>
     </body>    
 </html>
